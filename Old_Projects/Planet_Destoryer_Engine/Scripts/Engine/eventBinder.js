c199bd5a974dd6eb0bb1e64e584e8e612b090f08
James <jjack7543@gmail.com>
2021-11-23 11:11:59 -0500

    Cleaned up website

--- Old_Projects/Planet_Destoryer_Engine/Scripts/Engine/eventBinder.js
+++
@@ -1,5 +0,0 @@
-function bindEvents() {
-window.addEventListener('DOMContentLoaded', ExecuteAfterDOM);
-document.addEventListener("keydown",keyDownHandler, false);	
-document.addEventListener("keyup",keyUpHandler, false);
-}
